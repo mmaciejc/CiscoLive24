f86379b9e485bf5c1f1b1242c1648072109a8051
mmaciejc@cisco.com <mmaciejc@cisco.com>
2024-02-04 22:41:53 +0100

    initial release

--- resource-objects.tf
+++ resource-objects.tf
@@ -3,29 +3,20 @@
 ###########################################
 
 resource "fmc_host_objects" "server1" {
+
     name        = "Server_1"
     value       = var.server_1_ip
     description = "Managed by Terraform"
 }
+
 resource "fmc_host_objects" "server2" {
     name        = "Server_2"
     value       = var.server_2_ip
     description = "Managed by Terraform"
 }
 
-resource "fmc_network_objects" "network1" {
-
-  # Mandatory
-  name        = "Inside_LAN"
-  value       = "10.0.20.0/24"
-
-  # Optional
-  description = "Managed by Terraform"
-}
-
 resource "fmc_port_objects" "API_HTTP" {
 
-  # Mandatory
   name        = "API_HTTP"
   port        = "8080"
   protocol    = "TCP"
